4d542b16a864364e1504daab05340eba31705ccc
aslmt <megatechmuse@outlook.com>
2024-02-25 13:57:44 +0800

    code clean

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,7 @@
 mod ping;
 
-use actix_web::{App, Responder, web};
-use actix_web::dev::{ServiceFactory, ServiceRequest};
+use actix_web::{Responder, web};
+use actix_web::dev::{ServiceFactory};
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/api")
